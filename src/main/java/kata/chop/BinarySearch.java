7fc8d2e408533d112a8a1ad12b8f52a9afcbd4e8
ROQUIGNY Gabriel <gabriel@roquigny.eu>
2015-09-23 07:55:26 +0200

    First day
    - simple search

--- src/main/java/kata/chop/BinarySearch.java
+++ src/main/java/kata/chop/BinarySearch.java
@@ -1,7 +1,5 @@
 package kata.chop;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.List;
 
 /**
@@ -13,11 +11,36 @@ public class BinarySearch {
      * Give the index of the value to search into the given list.
      *
      * @param valueToSearch  value to search.
-     * @param listIntoSearch given list.
+     * @param listIntoSearch given sorted list.
      *
      * @return index if the value is find, -1 else.
      */
     public int chop(Integer valueToSearch, List<Integer> listIntoSearch) {
-        throw new NotImplementedException();
+        int beginIndex = 0;
+        int endIndex = listIntoSearch.size() - 1;
+
+        if (listIntoSearch.size() == 0) {
+            return -1;
+        }
+
+        if (valueToSearch.equals(listIntoSearch.get(beginIndex))) {
+            return beginIndex;
+        }
+        if (valueToSearch.equals(listIntoSearch.get(endIndex))) {
+            return endIndex;
+        }
+
+        while (beginIndex + 1 < endIndex) {
+            int mediumIndex = (endIndex + beginIndex) / 2;
+            if (valueToSearch.equals(listIntoSearch.get(mediumIndex))) {
+                return mediumIndex;
+            }
+            if (valueToSearch.compareTo(listIntoSearch.get(mediumIndex)) > 0) {
+                beginIndex = mediumIndex;
+            } else {
+                endIndex = mediumIndex;
+            }
+        }
+        return -1;
     }
 }
