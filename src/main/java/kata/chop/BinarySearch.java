065101bbb0bc2caa661797f2d3ed11b99b285f12
ROQUIGNY Gabriel <gabriel@roquigny.eu>
2015-09-29 08:07:25 +0200

    Second day

--- src/main/java/kata/chop/BinarySearch.java
+++ src/main/java/kata/chop/BinarySearch.java
@@ -1,7 +1,5 @@
 package kata.chop;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.List;
 
 /**
@@ -18,6 +16,37 @@ public class BinarySearch {
      * @return index if the value is find, -1 else.
      */
     public int chop(Integer valueToSearch, List<Integer> listIntoSearch) {
-        throw new NotImplementedException();
+        if (listIntoSearch.size() < 1) {
+            // Empty list
+            return -1;
+        }
+        if (valueToSearch.equals(listIntoSearch.get(0))) {
+            // First element match
+            return 0;
+        }
+        if (valueToSearch.equals(
+                listIntoSearch.get(listIntoSearch.size() - 1))) {
+            // Last element match
+            return listIntoSearch.size() - 1;
+        }
+        return recurciveChop(valueToSearch, listIntoSearch, 0,
+                listIntoSearch.size() - 1);
+    }
+
+    private int recurciveChop(Integer valueToSearch, List<Integer> listIntoSearch, int begin, int end) {
+        int middle = (begin + end) / 2;
+        if (middle == begin || middle == end) {
+            // begin == end + 1
+            return -1;
+        }
+        if (valueToSearch.equals(listIntoSearch.get(middle))) {
+            // Middle element match
+            return middle;
+        }
+        if (valueToSearch.compareTo(listIntoSearch.get(middle)) > 0) {
+            return recurciveChop(valueToSearch, listIntoSearch, middle, end);
+        } else {
+            return recurciveChop(valueToSearch, listIntoSearch, begin, middle);
+        }
     }
 }
